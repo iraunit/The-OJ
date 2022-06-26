abda4b75b35f5a931985748afa27823472d2b23a
Raunit <ramgopal10258@gmail.com>
2022-06-27 02:48:25 +0530

    Login

--- The_OJ/settings.py
+++ The_OJ/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)y9w9(=s^gw26h_zr%&%mcib^w@nrltsg5u+udo%t9kqmrd#k'
+
+
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
