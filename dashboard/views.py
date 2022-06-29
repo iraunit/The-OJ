ca10fe3dc05de0beb0d498c770e2be1e1b9cceb8
Raunit Verma <ramgopal10258@gmail.com>
2022-06-29 06:11:24 +0530

    adding db1

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,15 @@
+import imp
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
 # Create your views here.
-
+@csrf_exempt
 @login_required(login_url='/login')
-def home(request):
+def home(request,id=0):
      id=request.user.id
      user = User.objects.get(id=id)
      user_email = user.first_name + " "+user.last_name
@@ -14,3 +18,4 @@ def home(request):
      return render(request,'home.html',{
           "user_email":user_email  
      })
+
