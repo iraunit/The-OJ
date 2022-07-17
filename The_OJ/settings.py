05681d4e0998f863efa8d9845194fddb8fe1a553
Raunit Verma <ramgopal10258@gmail.com>
2022-07-18 02:20:47 +0530

    before deployment2

--- The_OJ/settings.py
+++ The_OJ/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 
+from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR + 'staticfiles')
+
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
